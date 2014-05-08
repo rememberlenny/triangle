4553e02d0de3599e52063e2560728dcaec6aa575
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-08 19:54:44 -0400

    Change the template

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Atriangle::Application.routes.draw do
-  get "pages/home"
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
