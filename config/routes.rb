2a3d40a4f1d47a7d4334b018499df381b6a1560a
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-08 19:52:09 -0400

    Change the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Atriangle::Application.routes.draw do
-  # get "pages/Home"
+  get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
