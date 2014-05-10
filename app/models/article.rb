38f9f2c005687a4931d0867de9e2f94d6cd23f70
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-10 11:59:49 -0400

    Add the word

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,12 +10,12 @@ class Article < ActiveRecord::Base
     words.each do |word|
       locals = Location.where(city_name: word)
       locals.each do |local|
-        review_all_city_results(local)
+        review_all_city_results(local, word)
       end
     end
   end
 
-  def self.review_all_city_results(local)
+  def self.review_all_city_results(local, word)
     if( local != nil)
       if( local.city_name != nil)
         puts "Found: " + word + ", a city in " + local.subdivision_name + ", " + local.country_name
