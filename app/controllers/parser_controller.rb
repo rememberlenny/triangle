ca2a066107ebb2175df8bd8f28374f2db108935e
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-09 21:03:37 -0400

    Create meta code for locations loop and check_parent_locations

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,4 +1,35 @@
 class ParserController < ApplicationController
   def locations
+    @text = 'thetext'
+    document_words = @text.split(' ')
+    # Set i_word and count
+    foreach document_words as document_word
+      #increment i_word
+      foreach countries as country
+        if( country == document_word)
+          check_parent_location(country)
+        end
+        foreach states as state
+          if( state == document_word)
+            check_parent_location(state)
+          end
+          foreach cities as city
+            if( city == document_word)
+              check_parent_location(city)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def check_parent_location(location)
+    if( city == location->type)
+
+    else( state == location->type)
+
+    else( country == location->type)
+
+    end
   end
 end
