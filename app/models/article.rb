5109f199fe46388a6d716a75cc060639ca62ee31
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-10 11:52:40 -0400

    Create a nil check before the local'

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,8 +9,10 @@ class Article < ActiveRecord::Base
   def self.check_for_location(words)
     words.each do |word|
       local = Location.find_by(city_name: word)
-      if( local.city_name != nil)
-        puts "Found: " + word + ", a city in " + local.subdivision_name + ", " + local.country_name
+      if( local != nil)
+        if( local.city_name != nil)
+          puts "Found: " + word + ", a city in " + local.subdivision_name + ", " + local.country_name
+        end
       end
     end
   end
