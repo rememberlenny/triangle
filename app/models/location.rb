0b3a57a75310ee94c65514add02424c9fe9ebdc0
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-10 11:15:45 -0400

    Solidify the Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,19 @@
 class Location < ActiveRecord::Base
+
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      Location.create! row.to_hash
+    SmarterCSV.process(file.tempfile, {
+    :key_mapping => {
+      :geoname_id => :geoname_id,
+      :continent_code => :continent_code,
+      :continent_name => :continent_name,
+      :country_iso_code => :country_iso_code,
+      :country_name => :country_name,
+      :subdivision_iso_code => :subdivision_iso_code,
+      :subdivision_name => :subdivision_name,
+      :city_name => :city_name,
+      :time_zone => :time_zone,
+    }}) do |location|
+      Location.create(location)
     end
   end
 end
