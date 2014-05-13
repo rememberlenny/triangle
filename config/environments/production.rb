03b06e1e8762c6a4a973ebb97b286d283eea00c3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 00:39:29 -0400

    Try resolving precompile issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Atriangle::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( vendor/modernizr.js )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
