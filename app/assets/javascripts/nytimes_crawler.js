cad4590d43b0f9c63be80dd53ceb2fe2adf7b209
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 20:20:36 -0400

    Create NY times crawler

---
+++ app/assets/javascripts/nytimes_crawler.js
@@ -0,0 +1,13 @@
+// Runs in console
+/*
+var currentArr = [];
+$$('#main .story').each(function(i, item){
+  var $this   = $$($$(this));
+  var date    = Date();
+  var title   = $this.find('h3').innerText;
+  var content = $this.find('.summary').innerText;
+  var currentObj = {url: url, title: title, content: content}
+  currentArr = currentArr.push(currentObj);
+});
+myRootRef.push(currentArr);
+*/
