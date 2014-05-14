1b947fe3949352198d075e8205d9c8b81df3ca79
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 21:38:50 -0400

    Script to run discriminates against URL not matched

--- app/assets/javascripts/nytimes_crawler.js
+++ app/assets/javascripts/nytimes_crawler.js
@@ -3,15 +3,16 @@ var currentArr = [];
 $$('#main .story:not(.advertisement):not(.autosStory)').each(function(i, item){
   var $this       = $$(item);
   var date        = Date();
-  var title       = $$(item).find('h2, h3, h5').text();
+  var title       = $$(item).find('h2, h3, h5, h6:last-child').text();
   var content     = $$(item).find('.summary').html();
-  var url         = $$(item).find('h2 a, h3 a, h5 a').attr('href');
+  var url         = $$(item).find('h2 a, h3 a, h5 a, h6:last-child a').attr('href');
+  var currentObj  = {};
   if(content !== undefined){
-    var currentObj  = {
-                      url: url,
-                      title: title,
-                      content: content
-                    };
+    currentObj.url  = url;
+    currentObj.title = title;
+    if(content !== undefined){
+      currentObj.content = content;
+    }
     currentArr.push(currentObj);
     console.log(currentObj);
   }
