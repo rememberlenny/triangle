2affa06d3ef8ce14c2f56a7ada7a61706b2b0257
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 20:44:50 -0400

    Get NYtimes homepage crawler to pull object

--- app/assets/javascripts/nytimes_crawler.js
+++ app/assets/javascripts/nytimes_crawler.js
@@ -1,13 +1,21 @@
 // Runs in console
-/*
 var currentArr = [];
-$$('#main .story').each(function(i, item){
-  var $this   = $$($$(this));
-  var date    = Date();
-  var title   = $this.find('h3').innerText;
-  var content = $this.find('.summary').innerText;
-  var currentObj = {url: url, title: title, content: content}
-  currentArr = currentArr.push(currentObj);
+$$('#main .story:not(.advertisement):not(.autosStory)').each(function(i, item){
+  var $this       = $$(item);
+  var date        = Date();
+  var title       = $$(item).find('h3, h5').text();
+  var content     = $$(item).find('.summary').html();
+  var url         = $$(item).find('h3 a, h5 a').attr('href');
+  if(content !== undefined){
+    var currentObj  = {
+                      item: item,
+                      $item: $$(item),
+                      url: url,
+                      title: title,
+                      content: content
+                    };
+  }
+  currentArr.push(currentObj);
+  console.log(currentObj);
 });
 myRootRef.push(currentArr);
-*/
