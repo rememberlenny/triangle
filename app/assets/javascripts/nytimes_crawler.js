cf73400c124acfbf94f14db34e88a6443b3453c4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 21:39:25 -0400

    Name the scraper function

--- app/assets/javascripts/nytimes_crawler.js
+++ app/assets/javascripts/nytimes_crawler.js
@@ -1,20 +1,22 @@
-// Runs in console
-var currentArr = [];
-$$('#main .story:not(.advertisement):not(.autosStory)').each(function(i, item){
-  var $this       = $$(item);
-  var date        = Date();
-  var title       = $$(item).find('h2, h3, h5, h6:last-child').text();
-  var content     = $$(item).find('.summary').html();
-  var url         = $$(item).find('h2 a, h3 a, h5 a, h6:last-child a').attr('href');
-  var currentObj  = {};
-  if(content !== undefined){
-    currentObj.url  = url;
-    currentObj.title = title;
+function scrape_nytimes_homepage(){
+  // Runs in console
+  var currentArr = [];
+  $$('#main .story:not(.advertisement):not(.autosStory)').each(function(i, item){
+    var $this       = $$(item);
+    var date        = Date();
+    var title       = $$(item).find('h2, h3, h5, h6:last-child').text();
+    var content     = $$(item).find('.summary').html();
+    var url         = $$(item).find('h2 a, h3 a, h5 a, h6:last-child a').attr('href');
+    var currentObj  = {};
     if(content !== undefined){
-      currentObj.content = content;
+      currentObj.url  = url;
+      currentObj.title = title;
+      if(content !== undefined){
+        currentObj.content = content;
+      }
+      currentArr.push(currentObj);
+      console.log(currentObj);
     }
-    currentArr.push(currentObj);
-    console.log(currentObj);
-  }
-});
-myRootRef.push(currentArr);
+  });
+  myRootRef.push(currentArr);
+}
