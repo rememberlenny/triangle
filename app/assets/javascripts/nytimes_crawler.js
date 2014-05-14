1dcea5ea3b012ed3f0d917e1a20dd6add9071087
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-13 20:46:52 -0400

    Add the first link wth h2

--- app/assets/javascripts/nytimes_crawler.js
+++ app/assets/javascripts/nytimes_crawler.js
@@ -3,19 +3,17 @@ var currentArr = [];
 $$('#main .story:not(.advertisement):not(.autosStory)').each(function(i, item){
   var $this       = $$(item);
   var date        = Date();
-  var title       = $$(item).find('h3, h5').text();
+  var title       = $$(item).find('h2, h3, h5').text();
   var content     = $$(item).find('.summary').html();
-  var url         = $$(item).find('h3 a, h5 a').attr('href');
+  var url         = $$(item).find('h2 a, h3 a, h5 a').attr('href');
   if(content !== undefined){
     var currentObj  = {
-                      item: item,
-                      $item: $$(item),
                       url: url,
                       title: title,
                       content: content
                     };
+    currentArr.push(currentObj);
+    console.log(currentObj);
   }
-  currentArr.push(currentObj);
-  console.log(currentObj);
 });
 myRootRef.push(currentArr);
