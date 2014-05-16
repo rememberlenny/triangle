24690a7bfea07c26b8493250735222e268e91d8f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-05-16 16:30:39 -0400

    Add image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
-      # user.oauth_token = auth.credentials.token
-      # user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      # user.save!
+      user.password = Devise.friendly_token[0,20]
+      user.image = auth.info.image # assuming the user model has an image
+
     end
   end
 
@@ -41,19 +41,6 @@ class User < ActiveRecord::Base
 
   def password_required?
     super && provider.blank?
-  end
-
-  def self.find_for_facebook_oauth(auth)
-
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-
-    end
   end
 
   def update_with_password(params, *options)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516170907) do
+ActiveRecord::Schema.define(version: 20140516203018) do
 
   create_table "articles", force: true do |t|
     t.text     "content"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 20140516170907) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "guest"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
