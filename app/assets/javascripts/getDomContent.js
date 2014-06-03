4d4b1f41467d39ac65a9b070e2bc62e81a262b7f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-02 22:53:56 -0400

    Create module for querying dom and saving all nodes

---
+++ app/assets/javascripts/getDomContent.js
@@ -0,0 +1,24 @@
+var GetDom = function(saveUrl, processUrl){
+  this.blastOff(saveUrl, processUrl);
+};
+
+GetDom.prototype.blastOff = function(saveUrl, processUrl){
+  var theDom;
+  if (document.querySelectorAll){
+    console.log('Using querySelectorAll');
+    // This is more performant
+    theDom = document.querySelectorAll("html");
+  } else {
+    console.log('Using getElementsByTagname');
+    // This works in all browsers
+    theDom = document.getElementsByTagName("html");
+  }
+  this.saveAllNodes(theDom, saveUrl, processUrl);
+}
+
+GetDom.prototype.saveAllNodes = function(theDom, saveUrl, processUrl){
+  var contentToProcess = [theDom, processUrl];
+  // url to query = saveUrl,
+  // content to pass = contentToProcess,
+  requestToSave = new XMLHttpRequest();
+}
