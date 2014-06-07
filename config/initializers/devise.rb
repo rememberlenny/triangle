16c774fe2afcc8428b4990161e61cd2a79776a76
rememberlenny <leonard_bogdonoff@condenast.com>
2014-06-07 17:55:47 -0400

    Add the env var dependency

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,6 +231,10 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :facebook, ENV['NEWS_TRIANGLE_FB_KEY'], ENV['NEWS_TRIANGLE_FB_SECRET']
+  config.omniauth :twitter, ENV['NEWS_TRIANGLE_TW_KEY'], ENV['NEWS_TRIANGLE_TW_SECRET']
+  config.omniauth :linked_in, ENV['NEWS_TRIANGLE_LI_KEY'], ENV['NEWS_TRIANGLE_LI_SECRET']
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
