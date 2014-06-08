fde62d16e2e779d2e03e8516ece783b0d2b66596
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-08 09:12:15 -0400

    Add the social booleans to the user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607215158) do
+ActiveRecord::Schema.define(version: 20140608131154) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20140607215158) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "is_facebook"
+    t.boolean  "is_linkedin"
+    t.boolean  "is_twitter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
