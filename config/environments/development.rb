b1494f068cb2363da7c75f46c6655662fdcbdb13
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-10 00:33:29 -0400

    Update dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,12 +30,12 @@ Newshack::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
+    address: 'smtp.gmail.com',
     port: '587',
     enable_starttls_auto: true,
     user_name: ENV['NT_EMAIL_USER'],
     password: ENV['NT_EMAIL_PASS'],
     :authentication => 'plain',
-    :domain => 'newstriangle.herokuapp.com'
+    :domain => 'atriangle.com'
   }
 end
