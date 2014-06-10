e59feb37085496bb174aedd0a15f5ceaa09fc746
RememberLenny <leonard_bogdonoff@condenast.com>
2014-06-10 19:30:38 -0400

    Change the hash to a text attr

--- db/migrate/20140610125701_add_fields_to_user.rb
+++ db/migrate/20140610125701_add_fields_to_user.rb
@@ -1,12 +1,12 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
     # https://github.com/skorks/omniauth-linkedin
-    add_column :users, :auth_hash_linkedin, :string
+    add_column :users, :auth_hash_linkedin, :text
 
     # https://github.com/mkdynamic/omniauth-facebook
-    add_column :users, :auth_hash_facebook, :string
+    add_column :users, :auth_hash_facebook, :text
 
     # https://github.com/arunagw/omniauth-twitter
-    add_column :users, :auth_hash_twitter, :string
+    add_column :users, :auth_hash_twitter, :text
   end
 end
